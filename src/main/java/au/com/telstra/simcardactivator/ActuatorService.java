ca561e7831048f41e7c889f456f1bdb619bec5a3
naerae100 <pradipdo2025@gmail.com>
2025-11-03 10:38:29 +1100

    Add debugging logs to ActuatorService for troubleshooting

--- src/main/java/au/com/telstra/simcardactivator/ActuatorService.java
+++ src/main/java/au/com/telstra/simcardactivator/ActuatorService.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -17,7 +18,7 @@ public class ActuatorService {
         this.restTemplate = restTemplate;
     }
     
-    public ActuatorResponse activateSimCard(String iccid) {
+    public ActuatorResponse activateSimCard(String iccid) throws RestClientException {
         ActuatorRequest request = new ActuatorRequest(iccid);
         
         HttpHeaders headers = new HttpHeaders();
@@ -25,7 +26,17 @@ public class ActuatorService {
         
         HttpEntity<ActuatorRequest> entity = new HttpEntity<>(request, headers);
         
-        return restTemplate.postForObject(ACTUATOR_URL, entity, ActuatorResponse.class);
+        System.out.println("Calling actuator service at: " + ACTUATOR_URL);
+        System.out.println("Request payload: {\"iccid\": \"" + iccid + "\"}");
+        
+        try {
+            ActuatorResponse response = restTemplate.postForObject(ACTUATOR_URL, entity, ActuatorResponse.class);
+            System.out.println("Actuator response: {\"success\": " + (response != null ? response.isSuccess() : "null") + "}");
+            return response;
+        } catch (RestClientException e) {
+            System.err.println("Error calling actuator service: " + e.getMessage());
+            throw e;
+        }
     }
 }
 
